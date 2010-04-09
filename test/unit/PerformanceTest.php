5ba41b966d32e001183f36674b2c9ca20bdd0f91
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-04-09 21:58:43 +0200

    improved performance test

--- test/unit/PerformanceTest.php
+++ test/unit/PerformanceTest.php
@@ -3,36 +3,44 @@ require_once dirname(__FILE__).'/../../vendor/lime.php';
 require_once dirname(__FILE__).'/../../lib/phpHtmlWriter.php';
 require_once dirname(__FILE__).'/phpHtmlWriterTestHelper.php';
 
-$t = new lime_test(1);
+$t = new lime_test(3);
 
 $view = new phpHtmlWriter();
 $nbTests  = 50;
 $nbTags   = 10;
 
+$t->is($view->tag('div#my_id', 'content'), '<div id="my_id">content</div>', 'Writer is ready');
+
 $start = microtime(true);
 
-for($i=0; $i<$nbTests; ++$i)
+for($iterator=0; $iterator<$nbTests; ++$iterator)
 {
-  $view->tag('div#my_id',
-    $view->tag('ul.my_class',
-      $view->tag('li', 'some content').
+  $view->tag('div#my_id'.$iterator,
+    $view->tag('ul.my_class'.$iterator,
+      $view->tag('li', 'some content '.$iterator).
       $view->tag('li',
-        $view->tag('input value="my value"')
+        $view->tag('input value="my value '.$iterator.'"')
       ).
       $view->tag('li',
         $view->tag('p', 'some content')
       )
-    )
+    ).
+    $view->open('div#my_id.my_class').
+      'some text'.
+      $view->tag('br').
+    $view->close('div')
   );
-  $view->open('div#my_id.my_class');
-  $view->close('div');
-  $view->tag('br');
 }
 
-$time = microtime(true) - $start;
+$time = 1000 * (microtime(true) - $start);
 
 $t->pass(sprintf(
-  'Time to render %d nested tags: %0.2f seconds',
+  'Time to render %d nested tags: %0.2f milliseconds',
   $nbTests * $nbTags,
   $time
+));
+
+$t->pass(sprintf(
+  'Time per tag: %0.2f milliseconds',
+  $time/($nbTests * $nbTags)
 ));
